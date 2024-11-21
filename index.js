3d6a887970deef7b0463c9662884c84b72531ece
javapak24 <ajava2424@gmail.com>
2024-11-20 23:53:50 -0500

    .

--- index.js
+++ index.js
@@ -3,13 +3,47 @@ const fileUpload = require('express-fileupload');
 const { S3Client, ListObjectsV2Command, PutObjectCommand } = require('@aws-sdk/client-s3');
 const cors = require('cors')
 const fs = require('fs')
+const path = require('path')
+
+
+
+
 
 const app = express()
 app.use(cors())
 
+const s3Client = new S3Client({
+    region: 'us-east-1',
+    endpoint: 'http://localhost:4566',
+    forcePathStyle: true
+})
+
+const listObjectsParams = {
+    Bucket: 'bucket2-4'
+}
+
+listObjectsCmd = new ListObjectsV2Command(listObjectsParams)
+
 app.get('/', (req, res) => {
     res.send('Welcome to my API!');
 });
+
+app.get('/images', (req, res) => {
+    listObjectsParams = {
+        Bucket: 'bucket2-4'
+    }
+    s3Client.send(new ListObjectsV2Command(listObjectsParams))
+        .then((listObjectsResponse) => {
+            res.send(listObjectsResponse)
+    })
+})
+
+
+
+
+
+
+
 
 const PORT = process.env.PORT || 8080;
 
